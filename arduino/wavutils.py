05abd37a04d35f4bbc1fbc30e34853e263d4361e
Daniel Pok <polerizer@gmail.com>
2014-10-04 14:17:30 -0700

    added functions to wavutils.py

--- arduino/wavutils.py
+++ arduino/wavutils.py
@@ -1,4 +1,5 @@
 import wave
+from array import array
 from math import sin, pi
 
 def compose(pitches, rate, bpm):
@@ -7,17 +8,41 @@ def compose(pitches, rate, bpm):
 	for p in pitches:
 		retval += tone(pitch[p], beat, rate)
 	return retval
-def tone(freq, duration, rate):
-	return bytes(int(127.5*sin(x*freq*2*pi/rate)+127.5) for x in range(0, int(44100*duration)))
+def tone(freq, duration, rate, scale=255):
+	return bytes(int(scale/2*sin(x*freq*2*pi/rate)+scale/2) for x in range(0, int(rate*duration)))
 
 pitch={"C":261.63,"D":293.66, "E":329.63,"F":349.23,"G":392,"A":440,"B":493.88,".":0}
 
-def wav(filename, data, rate=44100, width=1):
-	if(type(data) != bytes):
-		data = bytes(max(0, min(255, int(x))) for x in data)
-	w = wave.open(filename, "wb")
-	w.setnchannels(1)
-	w.setsampwidth(width)
-	w.setframerate(rate)
-	w.writeframes(data)
-	w.close()
+def wav(filename, data, rate=44100, width=2, channels=1):
+	datarray = array("h")
+	max_val = (2**(width-1)) -1
+	min_val = -(2**(width-1))
+	for x in data:
+		datarray.append(max(min_val, min(max_val, int(x))))
+
+	with wave.open(filename, "wb") as w:
+		w.setnchannels(channels)
+		w.setsampwidth(width)
+		w.setframerate(rate)
+		w.writeframes(datarray)
+		w.close()
+
+def poll_12_bit(filename, rate, samples,val_func, finish_func=None, bias=0):
+	width=2
+	channels=1
+	data = []
+	for _ in samples:
+		data.append(val_func()-bias)
+	if finish_func: finish_func()
+	wav(filename, data, rate, width, channels)
+
+def print_hello():
+	print("hello")
+
+def tone_func(freq, rate, scale):
+	def tgen():
+		n = 0
+		yield int(scale/2*sin(n*freq*2*pi/rate)+scale/2)
+		n += 1
+	g = tgen()
+	return lambda: next(g) 
