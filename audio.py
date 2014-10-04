c3073b130f50ecb5d52e2dc5d85672cb4f950b49
Rohit Swamy <rohitswamy@gmail.com>
2014-10-04 01:26:15 -0700

    junk

--- audio.py
+++ audio.py
@@ -1,20 +1,34 @@
-import numpy
-import pylab
 import scipy.io.wavfile
+import matplotlib.pyplot as plt
 from numpy.fft import *
-from pylab import * 
 
 rate, data = scipy.io.wavfile.read('Clear Speech No Noise.wav')
 
-print data
-print rate
-print len(data)
+print (data)
+print (rate)
+print (len(data))
 
 f = fft(data)
 
-pylab.specgram(f)
-print f
+
+plt.plot(f)
+print (f)
 
 
 
 
+# fs, frames = scipy.io.wavfile.read('Clear Speech No Noise.wav')
+
+# channels = [
+#     np.array(frames[:, 0]),
+#     np.array(frames[:, 1])
+# ]
+
+# # generate specgram
+# Pxx, freqs, t, plot = pylab.specgram(
+#     channels[0],
+#     NFFT=4096, 
+#     Fs=44100, 
+#     detrend=pylab.detrend_none,
+#     window=pylab.window_hanning,
+#     noverlap=int(4096 * 0.5))
