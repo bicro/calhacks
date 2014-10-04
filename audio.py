00c81c77207f0a869399081a1ad62566b4a674ee
Rohit Swamy <rohitswamy@gmail.com>
2014-10-04 02:13:44 -0700

    sayan and beautiful code

--- audio.py
+++ audio.py
@@ -1,38 +1,45 @@
-import scipy.io.wavfile
-import matplotlib.pyplot as plt
 from numpy.fft import *
+from pylab import *
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import pylab
 
-from matplotlib import pyplot as plt
+def plotGraph(fftArray):
+	plt.plot(fftArray)
+	plt.show()
+
+def plotSpecgram(fftArray):
+	f = 10.
+	w = 2. * np.pi * f
+	time_interval = 100
+	samples = 5000
+	t = np.linspace(0, time_interval, samples)
+	Pxx, freqs, t, plot = pylab.specgram(fftArray, NFFT=128, Fs=44100, detrend=pylab.detrend_none, window=pylab.window_hanning, noverlap=int(128 * 0.5))
+	show()
 
 
-rate, data = scipy.io.wavfile.read('Clear Speech No Noise.wav')
+def main():
+	rate1, data1 = scipy.io.wavfile.read('Clear Speech No Noise.wav')
+	rate2, data2 = scipy.io.wavfile.read('Clear Speech With Noise.wav')
 
-print (data)
-print (rate)
-print (len(data))
+	print ("rate 1: " + str(rate1))
+	print ("rate 2: " + str(rate2))
 
-f = fft(data)
+	print ("data 1: " + str(data1))
+	print ("data 2: " + str(data2))
+
+	f1 = fft(data1)
+	f2 = fft(data2)
+
+	# plotGraph(f1)
+	# plotGraph(f2)
+	plotSpecgram(data1)
+
+if __name__ == "__main__":
+	main()
 
 
-#fft.freq
 
 
-plt.plot(f)
-plt.show()
-
-
-# fs, frames = scipy.io.wavfile.read('Clear Speech No Noise.wav')
-
-# channels = [
-#     np.array(frames[:, 0]),
-#     np.array(frames[:, 1])
-# ]
-
-# # generate specgram
-# Pxx, freqs, t, plot = pylab.specgram(
-#     channels[0],
-#     NFFT=4096, 
-#     Fs=44100, 
-#     detrend=pylab.detrend_none,
-#     window=pylab.window_hanning,
-#     noverlap=int(4096 * 0.5))
